c2907f156b2c6c8205266e06fa0c98db70f4355a
Amit Kafle <amitkafleofficial@gmail.com>
2021-10-22 00:03:30 +0545

    Changed cursor to pointer for menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,26 @@
-const mobile_menu_open_icon = document.querySelector('.nav__mobile');
-const mobile_menu_close_icon = document.querySelector('.nav__close');
-const nav = document.querySelector('.nav');
+const mobile_menu_open_icon = document.querySelector(".nav__mobile");
+const mobile_menu_close_icon = document.querySelector(".nav__close");
+const nav = document.querySelector(".nav");
 
-mobile_menu_open_icon.addEventListener("click", ()=>{
+mobile_menu_open_icon.addEventListener("click", () => {
+  nav.style.display = "block";
+  nav.style.animationName = "menu-open";
+});
+
+mobile_menu_close_icon.addEventListener("click", () => {
+  setTimeout(() => {
+    nav.style.display = "none";
+  }, 500);
+  nav.style.animationName = "menu-close";
+});
+
+window.addEventListener("resize", () => {
+  const width =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+  if (width > 768) {
     nav.style.display = "block";
-    nav.style.animationName = "menu-open"; 
-})
-
-mobile_menu_close_icon.addEventListener("click", () =>{
-    setTimeout(()=>{
-        nav.style.display = "none";
-    },500)
-    nav.style.animationName = "menu-close";
-})
+  } 
+  console.log(width);
+});
